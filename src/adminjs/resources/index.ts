05f8228b54d349dd1297498456dc2add53eb1694
pdro-h0 <pedro.pho792@gmail.com>
2023-06-03 16:59:24 -0300

    Upload de arquivos no Adminjs

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -2,7 +2,7 @@ import { ResourceWithOptions } from "adminjs"
 import { Category, Course, Episode } from "../../models"
 import { categoryResourceOptions } from "./category"
 import { courseResourceOptions } from "./course"
-import { episodeResourceOptions } from "./episode"
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode"
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -16,6 +16,7 @@ export const adminJsResources: ResourceWithOptions[] = [
   },
   {
     resource: Episode,
-    options: episodeResourceOptions
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
   },
 ]
