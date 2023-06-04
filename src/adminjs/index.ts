a78ca52cdac39f47d135f1087cb66141c2c65298
pdro-h0 <pedro.pho792@gmail.com>
2023-06-04 13:36:58 -0300

    Personalizando o Dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,7 +3,7 @@ import AdminJSExpress from '@adminjs/express'
 import AdminJSSequelize from '@adminjs/sequelize'
 import { sequelize } from '../database'
 import { adminJsResources } from './resources'
-import { User } from '../models'
+import { Course, User, Episode, Category } from '../models'
 import bcrypt from 'bcrypt'
 import { locale } from './locale'
 
@@ -13,6 +13,22 @@ export const adminJs = new AdminJS({
   databases: [sequelize],
   rootPath: '/admin',
   resources: adminJsResources,
+  dashboard: {
+    component: AdminJS.bundle('./components/Dashboard'),
+		handler: async (req, res, context) => {
+      const courses = await Course.count()
+      const episodes = await Episode.count()
+      const category = await Category.count()
+      const standardUsers = await User.count({ where: { role: 'user' } })
+
+      res.json({
+        'Cursos': courses,
+        'Episódios': episodes,
+        'Categorias': category,
+        'Usuários': standardUsers
+      })
+    },
+  },
   locale: locale,
   branding: {
     companyName: 'OneBitFlix',
