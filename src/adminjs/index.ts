54b29c8dbb2dca8ab48132d9065d6c9582148f8e
pdro-h0 <pedro.pho792@gmail.com>
2023-06-04 13:03:55 -0300

    Adicionando traduções

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,6 +5,7 @@ import { sequelize } from '../database'
 import { adminJsResources } from './resources'
 import { User } from '../models'
 import bcrypt from 'bcrypt'
+import { locale } from './locale'
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -12,6 +13,7 @@ export const adminJs = new AdminJS({
   databases: [sequelize],
   rootPath: '/admin',
   resources: adminJsResources,
+  locale: locale,
   branding: {
     companyName: 'OneBitFlix',
     logo: '/logoOnebitflix.svg',
