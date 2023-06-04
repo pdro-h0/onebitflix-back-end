311d88e2765250e0ae562a3aa78f7ed5e545ee79
pdro-h0 <pedro.pho792@gmail.com>
2023-06-04 17:52:20 -0300

    Obtendo informações do curso

---
+++ src/controllers/courseController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { courseService } from "../services/courseService";
+
+export const courseController = {
+    show: async (req: Request, res: Response)=>{
+        const { id } = req.params
+
+        try {
+            const course = await courseService.findByIdWithEpisodes(id)
+
+            return res.json(course)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+              }
+        }
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
+import { courseController } from './controllers/courseController'
 
 const router = express.Router()
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
+
+router.get('/courses/:id', courseController.show)
 
 
 export { router }
