9944b41a479bd3d5a8808c71b02cc4f52e2f3997
pdro-h0 <pedro.pho792@gmail.com>
2023-06-09 16:46:47 -0300

    Obtendo a lista de continuar assistindo

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+import { AuthenticatedRequest } from "../middlewares/auth";
+import { userService } from "../services/userService";
+
+export const usersController = {
+  // GET /users/current/watching
+  watching: async (req: AuthenticatedRequest, res: Response) => {
+    const { id } = req.user!
+
+    try {
+      const watching = await userService.getKeepWatchingList(id)
+      return res.json(watching)
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message })
+      }
+    }
+  }
+}
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,21 +1,83 @@
-import { User } from "../models"
-import { UserCreationAttributes } from "../models/User"
+import { User } from "../models";
+import { UserCreationAttributes } from "../models/User";
+import { EpisodeInstance } from '../models/Episode'
 
-export const userService ={
-    findByEmail: async (email: string) => {
-        const user = await User.findOne({
-            where:{
-                email: email
-            }
-        })
+function filterLastEpisodesByCourse(episodes: EpisodeInstance[]) {
+    const coursesOnList: number[] = []
+  
+    const lastEpisodes = episodes.reduce((currentList, episode) => {
+      if (!coursesOnList.includes(episode.courseId)) {
+        coursesOnList.push(episode.courseId)
+        currentList.push(episode)
+        return currentList
+      }
+  
+      const episodeFromSameCourse = currentList.find(ep => ep.courseId === episode.courseId)
+  
+      if (episodeFromSameCourse!.order > episode.order) return currentList
+  
+      const listWithoutEpisodeFromSameCourse = currentList.filter(ep => ep.courseId !== episode.courseId)
+      listWithoutEpisodeFromSameCourse.push(episode)
+  
+      return listWithoutEpisodeFromSameCourse
+    }, [] as EpisodeInstance[])
+  
+    return lastEpisodes
+  }
 
-        return user
-    },
-    
-    
-    create: async(attributes: UserCreationAttributes)=>{
-        const user = await User.create(attributes)
+export const userService = {
+  findByEmail: async (email: string) => {
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
 
-        return user
-    }
-}
+    return user;
+  },
+
+  create: async (attributes: UserCreationAttributes) => {
+    const user = await User.create(attributes);
+
+    return user;
+  },
+
+  getKeepWatchingList: async (id: number) => {
+    const userWithWatchingEpisodes = await User.findByPk(id, {
+      include: {
+        association: "Episodes",
+        attributes: [
+          "id",
+          "name",
+          "synopsis",
+          "order",
+          ["video_url", "videoUrl"],
+          ["seconds_long", "secondsLong"],
+          ["course_id", "courseId"],
+        ],
+        include: [
+          {
+            association: "Course",
+            attributes: [
+              "id",
+              "name",
+              "synopsis",
+              ["thumbnail_url", "thumbnailUrl"],
+            ],
+            as: "course",
+          },
+        ],
+        through: {
+          as: "watchTime",
+          attributes: ["seconds", ["updated_at", "updatedAt"]],
+        },
+      },
+    });
+
+    if (!userWithWatchingEpisodes) throw new Error("Usuário não encontrado.");
+    const keepWatchingList = filterLastEpisodesByCourse(userWithWatchingEpisodes.Episodes!)
+    // @ts-ignore
+    keepWatchingList.sort((a, b) => a.watchTime.updatedAt < b.watchTime.updatedAt ? 1 : -1)
+    return keepWatchingList
+  },
+};
